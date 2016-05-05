d51486be98b2dcc86bb2c94d2457c8f93a4a0bcc
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-05-05 15:56:32 -0500

    change civica digital dimensions

--- app/scripts/directives/cloud.js
+++ app/scripts/directives/cloud.js
@@ -255,18 +255,6 @@ function cloud(quality,pollutant,element) {
         var text_div = d3.selectAll("section.text_in").text("Pasa el mouse por encima de alguna de las nubes para conocer más detalles sobre los contaminantes.");
       }
 
-      svg.append("circle")        // attach a rectangle
-          .attr("cx", max_width/2)        // position the left of the rectangle
-          .attr("cy", max_height/2)         // position the top of the rectangle
-          .attr("r",max_width)
-          .attr("id","fill_circle")
-          .attr("fill","transparent")
-
-      if (pollutant=="none") {
-          console.log(svg.selectAll("circle"))
-          svg.selectAll("#fill_circle").attr("fill","white")
-      }
-
 }
 
 angular.module('calidadDelAire')
